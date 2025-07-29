04dc3290f3aefda6c69fc9109219a7124fe58e8a
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-29 16:11:46 +0200

    feat: 2. Run time for four parallel comprehensions

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -4,7 +4,7 @@
 import asyncio
 import time
 from typing import Callable
-from 1-async_comprehension import async_comprehension
+from ('1-async_comprehension') import async_comprehension
 
 async def measure_runtime() -> float:
     """Run async_comprehension 4 times in parallel and return the total runtime."""
