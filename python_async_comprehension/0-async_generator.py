57ec759e487c9cf097c594ef365c4180620f37fd
hedjouj <messaoui.elhadj@hotmail.fr>
2025-07-29 15:46:29 +0200

    feat: 0. Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 '''async generator'''
 
 import asyncio, random
+from typing import async_generator
 
-
-async def async_generator():
+async def async_generator() -> async_generator[float, None]:
     '''async genrator'''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random(0, 10)
+        yield random.uniform(0, 10)
