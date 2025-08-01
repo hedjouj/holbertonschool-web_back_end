86789e946e63f72e1ca7789f7c49824edb99ce4d
hedjouj <messaoui.elhadj@hotmail.fr>
2025-08-01 16:36:25 +0200

    fix: pycode

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+"""
+2-floor.py
+Module containing function to compute the floor of a float.
+"""
 import math
-''' return the floor of the float'''
 
 
 def floor(n: float) -> int:
-    '''return the floor of the float'''
+    """
+    Computes the floor of a float.
+    """
     return math.floor(n)
