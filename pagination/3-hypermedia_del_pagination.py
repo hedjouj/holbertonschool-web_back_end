98697038ec38bff66599dbcf0a3bd5d1f2f6a428
hedjouj <messaoui.elhadj@hotmail.fr>
2025-08-11 14:45:22 +0200

    feat: 3. Deletion-resilient hypermedia pagination

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]  # remove header
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Return deletion-resilient hypermedia pagination info.
+        """
+        assert isinstance(index, int) and index >= 0, "index must be a non-negative integer"
+        indexed_data = self.indexed_dataset()
+        assert index < len(indexed_data), "index out of range"
+
+        data = []
+        current_index = index
+        collected = 0
+
+        # Collect page_size elements, skipping missing indexes
+        while collected < page_size and current_index < max(indexed_data.keys()) + 1:
+            if current_index in indexed_data:
+                data.append(indexed_data[current_index])
+                collected += 1
+            current_index += 1
+
+        next_index = current_index
+
+        return {
+            "index": index,
+            "data": data,
+            "page_size": len(data),
+            "next_index": next_index
+        }
