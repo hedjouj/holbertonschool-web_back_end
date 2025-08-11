ecbfa2489d35eeaa9920724888159a057e3eb2f0
hedjouj <messaoui.elhadj@hotmail.fr>
2025-08-11 13:08:10 +0200

    feat: 1. Simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        if page and page_size >= len(self.dataset):
+            return []
+        pass
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """function that return two int arg"""
+    tuple = (page - 1) * page_size, page * page_size
+    return tuple
