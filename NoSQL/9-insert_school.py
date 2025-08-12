e27864b1799aa7d2a312e60c74d35ee66ad10182
hedjouj <messaoui.elhadj@hotmail.fr>
2025-08-12 13:07:30 +0000

    feat: 9. Insert a document in Python

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -4,5 +4,6 @@ from pymongo import MongoClient
 
 
 def insert_school(mongo_collection, **kwargs):
-    return new_school_id
-    
+    """insert un doc"""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
