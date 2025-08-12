2dbfac329b649174ffca1dc357fee5ede8c59eff
hedjouj <messaoui.elhadj@hotmail.fr>
2025-08-12 12:48:18 +0000

    feat: 8. List all documents in Python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
-client = pymongo.MongoClient("mongodb://localhost:27017/")
 
 def list_all(mongo_collection):
     return list(mongo_collection.find())
