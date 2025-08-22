7dcaf23edbac24f74c34af8b743e92f02b64b0ff
hedjouj <messaoui.elhadj@hotmail.fr>
2025-08-22 14:58:53 +0200

    feat: task

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,19 @@
 export default class Building {
-    constructor(sqft) {
-        if (typeof sqft != 'number'){
-            throw new TypeError ('sqft must be a number');
-        }
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this.sqft = sqft;
+  }
 
-        if (this.constructor != Building && this.evacuationwWarningMessage == undefined) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Sqft must be a number');
     }
-    get sqft() {
-        return this._sqft;
-    }
+    this._sqft = value;
+  }
 }
