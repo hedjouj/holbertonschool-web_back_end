bdfad8d145a68fab367f49974e8f06429c749818
hedjouj <messaoui.elhadj@hotmail.fr>
2025-08-26 11:10:54 +0200

    feat: 0. Keep every promise you make and only make promises you can keep

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
 export default function getResponseFromAPI() {
-    const maPromesse = new Promise((resolve, reject) => {
-        if (everythingisok) {
-            resolve("True");
-        }
-        else {
-            reject("False");
-        }
+    return new Promise((resolve, reject) => {
+        resolve();
     });
 }
